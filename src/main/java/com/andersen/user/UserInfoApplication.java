49796a7f21b3812a0668081bbea9ed52f5bcaed4
Andrei Vazovikov <andrei.vazovikov@gmail.com>
2021-04-22 11:25:32 +0300

    initial project configuration

---
+++ src/main/java/com/andersen/user/UserInfoApplication.java
@@ -0,0 +1,13 @@
+package com.andersen.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserInfoApplication.class, args);
+	}
+
+}
